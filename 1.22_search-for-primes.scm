5a8cad5257622e87c1cfd03d216e9bf548216e76
Oleg Khlevnov <oleg@khlevnov.ru>
2017-07-28 22:47:23 +0300

    Добавлен метод на проверку чисел на простоту в диапазоне

---
+++ 1.22_search-for-primes.scm
@@ -0,0 +1,37 @@
+(define (smallest-divisor n)
+    (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+    (= (remainder b a) 0))
+
+(define (prime? n)
+    (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+    (newline)
+    (display n)
+    (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+    (if (prime? n)
+        (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
+
+(define (search-for-primes start-number)
+    (define (iterative-search number found-numbers-count)
+        (if (< found-numbers-count 3)
+            (if (prime? number)
+                (and (timed-prime-test number)
+                     (iterative-search (+ number 1) (+ found-numbers-count 1)))
+                (iterative-search (+ number 1) found-numbers-count))))
+    (iterative-search (+ start-number 1) 0))
+
+(search-for-primes 1000000000000)
