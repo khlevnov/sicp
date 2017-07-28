104013dd40299a5ff8117f650d843f6676745362
Oleg Khlevnov <oleg@khlevnov.ru>
2017-07-29 00:55:01 +0300

    Упрощен подсчет следующего кандидата для простого числа

--- 1.23_next.scm
+++ 1.23_next.scm
@@ -1,10 +1,15 @@
 (define (smallest-divisor n)
     (find-divisor n 2))
 
+(define (next number)
+    (if (= number 2)
+        3
+        (+ number 2)))
+
 (define (find-divisor n test-divisor)
     (cond ((> (square test-divisor) n) n)
           ((divides? test-divisor n) test-divisor)
-          (else (find-divisor n (+ test-divisor 1)))))
+          (else (find-divisor n (next test-divisor)))))
 
 (define (divides? a b)
     (= (remainder b a) 0))
@@ -34,8 +39,8 @@
                 (iterative-search (+ number 1) found-numbers-count))))
     (iterative-search (+ start-number 1) 0))
 
-; Without next
-(search-for-primes 1000000000) ; 0.03
-(search-for-primes 10000000000) ; 0.09
-(search-for-primes 100000000000) ; 0.29
-(search-for-primes 1000000000000) ; 0.90
+; With next
+(search-for-primes 1000000000) ; 0.02
+(search-for-primes 10000000000) ; 0.06
+(search-for-primes 100000000000) ; 0.18
+(search-for-primes 1000000000000) ; 0.59
