96a92b2ae9dea1edbebac97e72c90a4ae19fd042
Oleg Khlevnov <oleg@khlevnov.ru>
2017-08-13 18:55:55 +0300

    Закончена реализация метода Симпсона

--- 1.29_simpson-method.scm
+++ 1.29_simpson-method.scm
@@ -1,5 +1,7 @@
 (define (cube x) (* x x x))
 
+(define (square x) (* x x))
+
 (define (simpsons f a b n)
     (define (h a b n)
         (/ (- b a) n))
@@ -7,19 +9,18 @@
     (define (y k)
         (f (+ a (* k (h a b n)))))
 
-    (define (next-y y i)
-        (cond (= i 0) y
-              (= i n) y
-              (= (remainder i 2) 0) (* 2 y)
-              (= (remainder i 2) 1) (* 4 y)))
+    (define (next k)
+        (cond ((= k 0) (y k))
+              ((= k n) (y k))
+              ((= (remainder k 2) 0) (* 2 (y k)))
+              ((= (remainder k 2) 1) (* 4 (y k)))))
 
-    (define (sum y i)
+    (define (sum item i)
         (if (> i n)
             0
-            (+ (next-y y i) (sum y (+ i 1)))))
+            (+ (next i) (sum (next i) (+ i 1)))))
 
-    (* (/ h 3) (sum (y 0 0))))
+    (* (/ (h a b n) 3) (sum (next 0) 0)))
 
-(simpsons cube 0 1 100)
-
-; h/3 [y0 + 4y1 + 2y2 + 4y3 + 2y4 ... + yn]
+; (simpsons cube 0 1 1000)
+(simpsons square 2 4 1000)
