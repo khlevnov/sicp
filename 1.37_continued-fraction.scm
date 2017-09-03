b4c3db33f48a8d086f5422db15d284f2725b494c
Oleg Khlevnov <oleg@khlevnov.ru>
2017-09-03 03:07:08 +0300

    Добавлена линейно-итеративная реализация бесконечноцепной дроби

--- 1.37_continued-fraction.scm
+++ 1.37_continued-fraction.scm
@@ -1,9 +1,22 @@
-(define (cont-frac n d k)
+(define (cont-frac-recursive n d k)
     (if (= k 0)
         (/ (n k) (d k))
         (/ (n k)
            (+ (d k) (cont-frac n d (- k 1))))))
 
-(/ 1 (cont-frac (lambda (i) 1.0)
+(/ 1 (cont-frac-recursive (lambda (i) 1.0)
+    (lambda (i) 1.0)
+    12))
+
+(define (cont-frac n d k)
+    (define (iter n d i result)
+        (if (= i 0)
+            result
+            (iter n d (- i 1) (if (= i k)
+                                  (/ (n i) (d i))
+                                  (/ (n i) (+ (d i) result))))))
+    (iter n d k 0))
+
+(cont-frac (lambda (i) 1.0)
            (lambda (i) 1.0)
-           11))
+           12)
