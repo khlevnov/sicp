2a80d8048db857aa150f56bb2e9475cd985543e5
Oleg Khlevnov <oleg@khlevnov.ru>
2017-11-19 22:39:35 +0300

    Add cons car cdr implementations

---
+++ 2.04_pairs-as-procedures.scm
@@ -0,0 +1,10 @@
+(define (cons x y)
+    (lambda (m) (m x y)))
+
+(define (car z)
+    (z (lambda (p q) p)))
+
+(define (cdr z)
+    (z (lambda (p q) q)))
+
+((lambda (p q) q) 1 2)
---
+++ 2.05_pairs-as-exps.scm
@@ -0,0 +1,21 @@
+(define (cons n m)
+    (* (expt 2 n) (expt 3 m)))
+
+(define (logx n base)
+    (if (= n base)
+        1
+        (+ 1 (logx (/ n base) base))))
+
+(define (car z)
+    (if (not (= (remainder z 3) 0))
+        (logx z 2)
+        (car (/ z 3))))
+
+(define (cdr z)
+    (if (not (= (remainder z 2) 0))
+        (logx z 3)
+        (cdr (/ z 2))))
+
+(define z (cons 4 7))
+(car z)
+(cdr z)
