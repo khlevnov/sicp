6d946de8d481ad0403e6b97c6f464ecb54aa0ddc
Oleg Khlevnov <oleg@khlevnov.ru>
2017-12-10 21:13:12 +0300

    Add deep-reverse

--- 2.27_deep-reverse.scm
+++ 2.27_deep-reverse.scm
@@ -1,14 +1,14 @@
 (define nil '())
 
 (define (deep-reverse items)
-    (define (reverse items)
-        (define (reverse-iter reveresed items)
-            (if (null? items)
-                reveresed
-                (reverse-iter (cons (car items) reveresed)
-                                    (cdr items))))
-        (reverse-iter nil items))
-    (reverse items))
+    (define (reverse-iter reveresed items)
+        (if (null? items)
+            reveresed
+            (reverse-iter (cons (if (pair? (car items))
+                                    (reverse-iter nil (car items))
+                                    (car items)) reveresed)
+                                (cdr items))))
+    (reverse-iter nil items))
 
-(define x (list (list 1 2) (list 3 4) (list 5 6)))
+(define x (list (list 1 2 (list 4 1) 5) (list 3 4 (list 7 8 9) 10 11) (list 5 6)))
 (deep-reverse x)
