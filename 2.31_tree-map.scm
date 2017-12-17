b392f0354ccbf9afe145bed60664440cd759d927
Oleg Khlevnov <oleg@khlevnov.ru>
2017-12-17 21:20:52 +0300

    Add tree-map

---
+++ 2.31_tree-map.scm
@@ -0,0 +1,13 @@
+(define (tree-map mapper tree)
+    (map (lambda (sub-tree)
+            (if (pair? sub-tree)
+                (tree-map mapper sub-tree)
+                (mapper sub-tree)))
+         tree))
+
+(define (square-tree tree) (tree-map square tree))
+
+(square-tree
+    (list 1
+        (list 2 (list 3 4) 5)
+        (list 6 7)))
