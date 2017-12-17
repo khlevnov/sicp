d53ab7fa69faa7949396aa9c6db8a3ca9a81d01e
Oleg Khlevnov <oleg@khlevnov.ru>
2017-12-17 19:53:18 +0300

    Add pair implementaton of binery mobile

--- 2.29_binary-mobile.scm
+++ 2.29_binary-mobile.scm
@@ -34,6 +34,24 @@
              (= (moment (left-branch mobile))
                 (moment (right-branch mobile))))))
 
+(define (make-mobile left right)
+    (cons left right))
+
+(define (make-branch length structure)
+    (cons length structure))
+
+(define (left-branch mobile)
+    (car mobile))
+
+(define (right-branch mobile)
+    (cdr mobile))
+
+(define (branch-length branch)
+    (car branch))
+
+(define (branch-structure branch)
+    (cdr branch))
+
 (define imbalanced-mobile (make-mobile
     (make-branch 3 (make-mobile
         (make-branch 3 1)
