b402fc3ad98133299d581ebd54fc0880bc41a6a2
Oleg Khlevnov <oleg@khlevnov.ru>
2017-12-17 20:21:11 +0300

    Add square-tree implementation

--- 2.30_square-tree.scm
+++ 2.30_square-tree.scm
@@ -6,13 +6,27 @@
           (else (cons (scale-tree (car tree) factor)
                       (scale-tree (cdr tree) factor)))))
 
-;(scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
+(define (scale-tree tree factor)
+    (map (lambda (sub-tree)
+            (if (pair? sub-tree)
+                (scale-tree sub-tree factor)
+                (* sub-tree factor)))
+         tree))
+
+(scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
 
 (define (square-tree tree)
     (cond ((null? tree) nil)
           ((not (pair? tree)) (square tree))
           (else (cons (square-tree (car tree))
                       (square-tree (cdr tree))))))
+
+(define (square-tree tree)
+    (map (lambda (sub-tree)
+            (if (pair? sub-tree)
+                (square-tree sub-tree)
+                (square sub-tree)))
+         tree))
 
 (square-tree
     (list 1
